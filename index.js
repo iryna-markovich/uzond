673f222c23e2cc5935cd3628b9c9ad0216598ab4
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-02 22:12:12 +0200

    test

---
+++ index.js
@@ -0,0 +1,1 @@
+setInterval(() => { console.log('dddddd') }, 10000)
--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export const formatDate = (dateString: string) => {
-  const [day, month, year] = dateString.split('/');
-  const formattedDateString = `${year}-${month}-${day}`;
-
-  return formattedDateString;
-}
