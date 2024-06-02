8dad74e6a17f297ec896925210c82238ec2501e0
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-02 16:27:02 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// import { Telegraf } from 'telegraf';
 import { about } from './commands';
 import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
@@ -14,31 +13,18 @@ const bot = new TelegramBot(TG_BOT_TOKEN, { polling: true });
 
 // bot.command('about', about());
 // bot.on('message', greeting());
-bot.on('message', (msg) => {
+const gre = (msg: any) => {
   const chatId = msg.chat.id;
   console.log(chatId, '------------')
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
-});
+
+}
+bot.on('message', gre);
 
 export default async function handle(req: VercelRequest, res: VercelResponse) {
   try {
-    // if (!VERCEL_URL) {
-    //   throw new Error('VERCEL_URL is not set.');
-    // }
-  
-    // const getWebhookInfo = await bot.telegram.getWebhookInfo();
 
-    // if (getWebhookInfo.url !== VERCEL_URL + '/api') {
-    //   await bot.telegram.deleteWebhook();
-    //   await bot.telegram.setWebhook(`${VERCEL_URL}/api`);
-    // }
-  
-    // if (req.method === 'POST') {
-    //   // await bot.handleUpdate(req.body as unknown as Update, res);
-    // } else {
-    //   res.status(200).json('Listening to bot events...');
-    // }
   } catch (e: any) {
     res.statusCode = 500;
     res.setHeader('Content-Type', 'text/html');
