bd8adce495060b0e8be7ca693b662db1f8e96b28
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-02 16:33:17 +0200

    removed unused

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
-import { about } from './commands';
-import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { Update } from 'telegraf/typings/core/types/typegram';
 import TelegramBot from 'node-telegram-bot-api';
 
 const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN || '';
-const ENVIRONMENT = process.env.NODE_ENV || '';
-const VERCEL_URL = `${process.env.VERCEL_URL}`;
 
-// const bot = new Telegraf(TG_BOT_TOKEN);
 const bot = new TelegramBot(TG_BOT_TOKEN, { polling: true });
 
-// bot.command('about', about());
-// bot.on('message', greeting());
 const gre = (msg: any) => {
   const chatId = msg.chat.id;
   console.log(chatId, '------------')
-  // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
-
 }
 bot.on('message', gre);
 
