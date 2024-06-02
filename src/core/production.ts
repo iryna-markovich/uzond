1bee5411725b49eba8a4cc26925f1f6c636e5438
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-02 15:42:13 +0200

    removed debug

--- src/core/production.ts
+++ src/core/production.ts
@@ -1,11 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import createDebug from 'debug';
 import { Context, Telegraf } from 'telegraf';
 import { Update } from 'telegraf/typings/core/types/typegram';
 
-const debug = createDebug('bot:dev');
-
-const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
 const VERCEL_URL = `${process.env.VERCEL_URL}`;
 
 const production = async (
@@ -13,18 +9,13 @@ const production = async (
   res: VercelResponse,
   bot: Telegraf<Context<Update>>,
 ) => {
-  debug('Bot runs in production mode');
-  debug(`setting webhook: ${VERCEL_URL}`);
-
   if (!VERCEL_URL) {
     throw new Error('VERCEL_URL is not set.');
   }
 
   const getWebhookInfo = await bot.telegram.getWebhookInfo();
   if (getWebhookInfo.url !== VERCEL_URL + '/api') {
-    debug(`deleting webhook ${VERCEL_URL}`);
     await bot.telegram.deleteWebhook();
-    debug(`setting webhook: ${VERCEL_URL}/api`);
     await bot.telegram.setWebhook(`${VERCEL_URL}/api`);
   }
 
@@ -33,6 +24,5 @@ const production = async (
   } else {
     res.status(200).json('Listening to bot events...');
   }
-  debug(`starting webhook on port: ${PORT}`);
 };
 export { production };
