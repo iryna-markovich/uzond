0c2c9a4fd3cbf7b3b773aa10e24e120b829afe25
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-02 17:32:56 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,18 @@ import { Telegraf } from 'telegraf';
 import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
+import TelegramBot from 'node-telegram-bot-api';
 
 const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
+const CHAT_ID = process.env.CHAT_ID || '';
 
 const bot = new Telegraf(TG_BOT_TOKEN);
+const bot2 = new TelegramBot(TG_BOT_TOKEN, { polling: true });
 
 // bot.command('about', about());
 bot.on('message', greeting());
+bot2.sendMessage(CHAT_ID, `📅 dia.pl/branch/5\nPosted ${new Date()}`);
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
