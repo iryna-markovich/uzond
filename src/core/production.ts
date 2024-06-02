f9f6c8f04f7a3c2b43070a92d7a068994c8ac24f
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-02 17:39:39 +0200

    test

--- src/core/production.ts
+++ src/core/production.ts
@@ -3,26 +3,20 @@ import { Context, Telegraf } from 'telegraf';
 import { Update } from 'telegraf/typings/core/types/typegram';
 import TelegramBot from 'node-telegram-bot-api';
 
-const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
-const VERCEL_URL = `${process.env.VERCEL_URL}`;
+// const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
+// const VERCEL_URL = `${process.env.VERCEL_URL}`;
 
 const production = async (
   req: VercelRequest,
   res: VercelResponse,
   bot: TelegramBot,
 ) => {
-  if (!VERCEL_URL) {
-    throw new Error('VERCEL_URL is not set.');
-  }
-
-  // const getWebhookInfo = await bot.telegram.getWebhookInfo();
-  // if (getWebhookInfo.url !== VERCEL_URL + '/api') {
-  //   await bot.telegram.deleteWebhook();
-  //   await bot.telegram.setWebhook(`${VERCEL_URL}/api`);
+  // if (!VERCEL_URL) {
+  //   throw new Error('VERCEL_URL is not set.');
   // }
 
   if (req.method === 'POST') {
-    // await bot.handleUpdate(req.body as unknown as Update, res);
+
   } else {
     res.status(200).json('Listening to bot events...');
   }
