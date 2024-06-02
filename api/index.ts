731ef180d5463b532925ee5835810e47876d3a0e
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-02 21:55:27 +0200

    test

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,21 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { startVercel } from '../src';
 
-setInterval(() => { 
- async function handle(req: VercelRequest, res: VercelResponse) {
-  try {
-    setInterval(() => { console.log('444') }, 60_000)
+export default async function handle(req: VercelRequest, res: VercelResponse) {
+  setInterval(async () => {
 
-    await startVercel(req, res)
-    setInterval(() => { console.log('5555') }, 60_000)
+    try {
+      setInterval(() => { console.log('444') }, 60_000)
 
-  } catch (e: any) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`<h1>Server Error</h1><p>${e.toString()}</p>`);
-    console.error(e.message);
-  }
+      await startVercel(req, res)
+      setInterval(() => { console.log('5555') }, 60_000)
+
+    } catch (e: any) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`<h1>Server Error</h1><p>${e.toString()}</p>`);
+      console.error(e.message);
+    }
+  }, 10000)
+
 }
-}, 10000)
