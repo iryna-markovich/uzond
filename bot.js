f30039f45e87c986b811af2b7ad9348e8b161e83
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-03 12:38:20 +0200

    Ready to run on heroku

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { formatDate, getDates } from './utils/index.js';
 
+
 const TOKEN = process.env.TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
 
