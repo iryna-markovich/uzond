1346f33812a40c2b8ea101c2c5dc1fb0a608b30c
Iryna Markovich <irina.markovich13@gmail.com>
2024-06-03 11:29:30 +0200

    test

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const formatDate = require('./util.js')
 const token = process.env.TOKEN;
 const chatId = process.env.CHAT_ID;
 
-export const suitableDateFrom = '2024-07-01'
-export const suitableDateTo = '2024-12-01'
+const suitableDateFrom = '2024-07-01'
+const suitableDateTo = '2024-12-01'
 
 let delay = 30_000;
 
--- util.js
+++ util.js
@@ -1,6 +1,8 @@
-export const formatDate = (dateString) => {
+const formatDate = (dateString) => {
   const [day, month, year] = dateString.split('/');
   const formattedDateString = `${year}-${month}-${day}`;
 
   return formattedDateString;
 }
+
+module.exports = formatDate;
